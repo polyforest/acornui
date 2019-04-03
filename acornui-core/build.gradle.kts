4eae8d426ccf8d746cf18ca7f00a3a3cc961ff4e
julia <julia@polyforest.com>
2019-04-02 17:40:25 -0700

    Refactor acornui-core to use 'basic' plugin

--- acornui-core/build.gradle.kts
+++ acornui-core/build.gradle.kts
@@ -15,78 +15,29 @@
  */
 
 plugins {
-    kotlin("multiplatform")
+    id("com.polyforest.acornui.basic")
     `maven-publish`
 }
 
-val KOTLIN_LANGUAGE_VERSION: String by extra
-val KOTLIN_JVM_TARGET: String by extra
 val MOCKITO_VERSION: String by extra
 val OBJENESIS_VERSION: String by extra
 kotlin {
-    js {
-        compilations.all {
-            kotlinOptions {
-                moduleKind = "amd"
-                sourceMap = true
-                sourceMapEmbedSources = "always"
-                main = "noCall"
-            }
-        }
-    }
-    jvm {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = KOTLIN_JVM_TARGET
-            }
-        }
-    }
-    targets.all {
-        compilations.all {
-            kotlinOptions {
-                languageVersion = KOTLIN_LANGUAGE_VERSION
-                apiVersion = KOTLIN_LANGUAGE_VERSION
-                verbose = true
-            }
-        }
-    }
-
     sourceSets {
         commonMain {
             dependencies {
-                implementation(kotlin("stdlib-common"))
                 implementation(project(":acornui-utils"))
             }
         }
         commonTest {
             dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
                 implementation(project(":acornui-test-utils"))
-            }
-        }
-        named("jvmMain") {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
             }
         }
         named("jvmTest") {
             dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
                 implementation(kotlin("reflect"))
                 implementation("org.mockito:mockito-core:$MOCKITO_VERSION")
                 implementation("org.objenesis:objenesis:$OBJENESIS_VERSION")
-            }
-        }
-        named("jsMain") {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-            }
-        }
-        named("jsTest") {
-            dependencies {
-                implementation(kotlin("test-js"))
             }
         }
     }
