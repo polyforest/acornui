0127d1fb2d19108e15a8b90999b1d85abd2e0ea4
julia <julia@polyforest.com>
2019-04-02 17:56:34 -0700

    Refactor acornui-lwjgl-backend to use 'basic' plugin

--- backends/acornui-lwjgl-backend/build.gradle.kts
+++ backends/acornui-lwjgl-backend/build.gradle.kts
@@ -15,58 +15,20 @@
  */
 
 plugins {
-    kotlin("multiplatform")
+    id("com.polyforest.acornui.basic")
     `maven-publish`
 }
 
-val KOTLIN_LANGUAGE_VERSION: String by extra
-val KOTLIN_JVM_TARGET: String by extra
 kotlin {
-    js {
-        compilations.all {
-            kotlinOptions {
-                moduleKind = "amd"
-                sourceMap = true
-                sourceMapEmbedSources = "always"
-                main = "noCall"
-            }
-        }
-    }
-    jvm {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = KOTLIN_JVM_TARGET
-            }
-        }
-    }
-    targets.all {
-        compilations.all {
-            kotlinOptions {
-                languageVersion = KOTLIN_LANGUAGE_VERSION
-                apiVersion = KOTLIN_LANGUAGE_VERSION
-                verbose = true
-            }
-        }
-    }
-
     sourceSets {
         commonMain {
             dependencies {
-                implementation(kotlin("stdlib-common"))
                 implementation(project(":acornui-core"))
                 implementation(project(":acornui-utils"))
             }
         }
-        commonTest {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
         named("jvmMain") {
             dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-
                 val LWJGL_VERSION: String by extra
                 val JORBIS_VERSION: String by extra
                 val JLAYER_VERSION: String by extra
@@ -91,22 +53,9 @@ kotlin {
                 val HAMCREST_VERSION: String by extra
                 val MOCKITO_VERSION: String by extra
                 val OBJENESIS_VERSION: String by extra
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
                 implementation("org.hamcrest:hamcrest-core:$HAMCREST_VERSION")
                 implementation("org.mockito:mockito-core:$MOCKITO_VERSION")
                 implementation("org.objenesis:objenesis:$OBJENESIS_VERSION")
-
-            }
-        }
-        named("jsMain") {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-            }
-        }
-        named("jsTest") {
-            dependencies {
-                implementation(kotlin("test-js"))
             }
         }
     }
