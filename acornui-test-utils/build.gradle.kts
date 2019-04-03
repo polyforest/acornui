4d98d4b860e38bb67a3b123fdbc8d886ba445a02
julia <julia@polyforest.com>
2019-04-02 17:49:00 -0700

    Refactor acornui-test-utils to use 'basic' plugin

--- acornui-test-utils/build.gradle.kts
+++ acornui-test-utils/build.gradle.kts
@@ -31,79 +31,27 @@
  */
 
 plugins {
-    kotlin("multiplatform")
+    id("com.polyforest.acornui.basic")
     `maven-publish`
 }
 
-val KOTLIN_LANGUAGE_VERSION: String by extra
-val KOTLIN_JVM_TARGET: String by extra
 kotlin {
-    js {
-        compilations.all {
-            kotlinOptions {
-                moduleKind = "amd"
-                sourceMap = true
-                sourceMapEmbedSources = "always"
-                main = "noCall"
-            }
-        }
-    }
-    jvm {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = KOTLIN_JVM_TARGET
-            }
-        }
-    }
-    targets.all {
-        compilations.all {
-            kotlinOptions {
-                languageVersion = KOTLIN_LANGUAGE_VERSION
-                apiVersion = KOTLIN_LANGUAGE_VERSION
-                verbose = true
-            }
-        }
-    }
-
     sourceSets {
         commonMain {
             dependencies {
-                implementation(kotlin("stdlib-common"))
-
                 implementation(kotlin("test-common"))
                 implementation(kotlin("test-annotations-common"))
                 
                 implementation(project(":acornui-utils"))
             }
         }
-        commonTest {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
         named("jvmMain") {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
-            }
-        }
-        named("jvmTest") {
             dependencies {
                 implementation(kotlin("test"))
                 implementation(kotlin("test-junit"))
             }
         }
         named("jsMain") {
-            dependencies {
-                implementation(kotlin("stdlib-js"))
-
-                implementation(kotlin("test-js"))
-            }
-        }
-        named("jsTest") {
             dependencies {
                 implementation(kotlin("test-js"))
             }
