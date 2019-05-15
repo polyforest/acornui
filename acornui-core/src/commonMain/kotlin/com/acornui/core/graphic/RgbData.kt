75e3918e2b95b748faf44457f9b140dce1bd8342
Nick <nick@polyforest.com>
2019-05-15 13:09:36 -0500

    RgbData private set properties

--- acornui-core/src/commonMain/kotlin/com/acornui/core/graphic/RgbData.kt
+++ acornui-core/src/commonMain/kotlin/com/acornui/core/graphic/RgbData.kt
@@ -28,57 +28,51 @@ class RgbData(
 		/**
 		 * The width of this image.
 		 */
-		width: Int,
+		initialWidth: Int,
 
 		/**
 		 * The height of this image.
 		 */
-		height: Int,
+		initialHeight: Int,
 
 		val hasAlpha: Boolean) {
 
-	private var _width = width
-	private var _height = height
+	var width = initialWidth
+		private set
 
-	val width: Int
-		get(): Int = _width
-
-	val height: Int
-		get(): Int = _height
+	var height = initialHeight
+		private set
 
 	val numBands: Int = if (hasAlpha) 4 else 3
 
-	private var _scanSize: Int = _width * numBands
-	private var _bytes: ByteArray = ByteArray(_width * _height * numBands)
+	var scanSize: Int = width * numBands
+		private set
 
-	val bytes: ByteArray
-		get(): ByteArray = _bytes
+	var bytes: ByteArray = ByteArray(width * height * numBands)
+		private set
 
-	val scanSize : Int
-		get(): Int = _scanSize
-
-	fun get(index: Int): Byte {
-		return _bytes[index]
+	operator fun get(index: Int): Byte {
+		return bytes[index]
 	}
 
 	operator fun set(index: Int, value: Byte) {
-		_bytes[index] = value
+		bytes[index] = value
 	}
 
 	val lastIndex: Int
-		get(): Int = _bytes.lastIndex
+		get(): Int = bytes.lastIndex
 
 	/**
 	 * @param out The color to populate.
 	 * @return The provided color object.
 	 */
 	fun getPixel(x: Int, y: Int, out: Color): Color {
-		var i = y * _scanSize + x * numBands
-		out.r = _bytes[i++].toFloatRange()
-		out.g = _bytes[i++].toFloatRange()
-		out.b = _bytes[i++].toFloatRange()
+		var i = y * scanSize + x * numBands
+		out.r = bytes[i++].toFloatRange()
+		out.g = bytes[i++].toFloatRange()
+		out.b = bytes[i++].toFloatRange()
 		if (hasAlpha) {
-			out.a = _bytes[i].toFloatRange()
+			out.a = bytes[i].toFloatRange()
 		} else {
 			out.a = 1f
 		}
@@ -90,20 +84,20 @@ class RgbData(
 	 */
 	fun getAlpha(x: Int, y: Int): Float {
 		if (!hasAlpha) return 1f
-		val i = y * _scanSize + x * numBands
-		return _bytes[i + 3].toFloatRange()
+		val i = y * scanSize + x * numBands
+		return bytes[i + 3].toFloatRange()
 	}
 
 	/**
 	 * Writes the color to the provided location.
 	 */
 	fun setPixel(x: Int, y: Int, color: ColorRo) {
-		var i = y * _scanSize + x * numBands
-		_bytes[i++] = (color.r * 255f).toByte()
-		_bytes[i++] = (color.g * 255f).toByte()
-		_bytes[i++] = (color.b * 255f).toByte()
+		var i = y * scanSize + x * numBands
+		bytes[i++] = (color.r * 255f).toByte()
+		bytes[i++] = (color.g * 255f).toByte()
+		bytes[i++] = (color.b * 255f).toByte()
 		if (hasAlpha) {
-			_bytes[i] = (color.a * 255f).toByte()
+			bytes[i] = (color.a * 255f).toByte()
 		}
 	}
 
@@ -111,8 +105,8 @@ class RgbData(
 	 * Fills the supplied area a solid color.
 	 */
 	fun fillRect(x: Int, y: Int, width: Int, height: Int, color: ColorRo) {
-		val lastY = minOf(_height, y + height) - 1
-		val lastX = minOf(_width, x + width) - 1
+		val lastY = minOf(this.height, y + height) - 1
+		val lastX = minOf(this.width, x + width) - 1
 		for (y2 in y..lastY) {
 			for (x2 in x..lastX) {
 				setPixel(x2, y2, color)
@@ -130,26 +124,26 @@ class RgbData(
 		val a = (color.a * 255f).toByte()
 		for (i in 0..lastIndex step numBands) {
 			var j = i
-			_bytes[j++] = r
-			_bytes[j++] = g
-			_bytes[j++] = b
+			bytes[j++] = r
+			bytes[j++] = g
+			bytes[j++] = b
 			if (hasAlpha) {
-				_bytes[j] = a
+				bytes[j] = a
 			}
 		}
 	}
 
 	private fun transferPixelTo(sourceX: Int, sourceY: Int, dest: RgbData, destX: Int, destY: Int) {
-		transferPixelTo(sourceX, sourceY, dest._bytes, dest._scanSize, dest.hasAlpha, dest.numBands, destX, destY)
+		transferPixelTo(sourceX, sourceY, dest.bytes, dest.scanSize, dest.hasAlpha, dest.numBands, destX, destY)
 	}
 	private fun transferPixelTo(sourceX: Int, sourceY: Int, destBytes: ByteArray, destScanSize: Int, destHasAlpha: Boolean, destNumBands: Int, destX: Int, destY: Int) {
-		var sourceIndex = sourceY * _scanSize + sourceX * numBands
+		var sourceIndex = sourceY * scanSize + sourceX * numBands
 		var destIndex = destY * destScanSize + destX * destNumBands
-		destBytes[destIndex++] = _bytes[sourceIndex++]
-		destBytes[destIndex++] = _bytes[sourceIndex++]
-		destBytes[destIndex++] = _bytes[sourceIndex++]
+		destBytes[destIndex++] = bytes[sourceIndex++]
+		destBytes[destIndex++] = bytes[sourceIndex++]
+		destBytes[destIndex++] = bytes[sourceIndex++]
 		if (hasAlpha && destHasAlpha) {
-			destBytes[destIndex] = _bytes[sourceIndex]
+			destBytes[destIndex] = bytes[sourceIndex]
 		} else if (destHasAlpha) {
 			destBytes[destIndex] = -1
 		}
@@ -171,45 +165,45 @@ class RgbData(
 	 * Rotates the image 90 degrees clockwise.
 	 */
 	fun rotate90CW() {
-		val newScanSize: Int = _height * numBands
-		val newBytes = ByteArray(_width * _height * numBands);
-		for (y in 0.._height - 1) {
-			val x2 = _height - 1 - y
-			for (x in 0.._width - 1) {
+		val newScanSize: Int = height * numBands
+		val newBytes = ByteArray(width * height * numBands);
+		for (y in 0..height - 1) {
+			val x2 = height - 1 - y
+			for (x in 0..width - 1) {
 				val y2 = x
 				transferPixelTo(x, y, newBytes, newScanSize, hasAlpha, numBands, x2, y2)
 			}
 		}
-		_bytes = newBytes
-		_scanSize = newScanSize
-		val tmp = _width
-		_width = _height
-		_height = tmp
+		bytes = newBytes
+		scanSize = newScanSize
+		val tmp = width
+		width = height
+		height = tmp
 	}
 
 	/**
 	 * Rotates the image 90 degrees clockwise.
 	 */
 	fun rotate90CCW() {
-		val newScanSize: Int = _height * numBands
-		val newBytes = ByteArray(_width * _height * numBands);
-		for (y in 0.._height - 1) {
+		val newScanSize: Int = height * numBands
+		val newBytes = ByteArray(width * height * numBands);
+		for (y in 0..height - 1) {
 			val x2 = y
-			for (x in 0.._width - 1) {
-				val y2 = _width - 1 - x
+			for (x in 0..width - 1) {
+				val y2 = width - 1 - x
 				transferPixelTo(x, y, newBytes, newScanSize, hasAlpha, numBands, x2, y2)
 			}
 		}
-		_bytes = newBytes
-		_scanSize = newScanSize
-		val tmp = _width
-		_width = _height
-		_height = tmp
+		bytes = newBytes
+		scanSize = newScanSize
+		val tmp = width
+		width = height
+		height = tmp
 	}
 
 	fun setRect(destX: Int, destY: Int, source: RgbData, sourceX: Int = 0, sourceY: Int = 0, width: Int = source.width - sourceX, height: Int = source.height - sourceY) {
-		val lastY = minOf(minOf(height, _height - destY), source.height - sourceY) - 1
-		val lastX = minOf(minOf(width, _width - destX), source.width - sourceX) - 1
+		val lastY = minOf(minOf(height, this.height - destY), source.height - sourceY) - 1
+		val lastX = minOf(minOf(width, this.width - destX), source.width - sourceX) - 1
 		for (y in 0..lastY) {
 			for (x in 0..lastX) {
 				source.transferPixelTo(sourceX + x, sourceY + y, this, destX + x, destY + y)
@@ -236,8 +230,8 @@ object RgbDataSerializer : To<RgbData>, From<RgbData> {
 
 	override fun read(reader: Reader): RgbData {
 		return RgbData(
-				width = reader.int("width")!!,
-				height = reader.int("height")!!,
+				initialWidth = reader.int("width")!!,
+				initialHeight = reader.int("height")!!,
 				hasAlpha = reader.bool("hasAlpha")!!
 		).apply {
 			val bytes = reader.byteArray("bytes")!!
