f1045633c34517230c224a70831cd0160d6e1bf9
Nick <nick@polyforest.com>
2019-05-31 08:45:47 -0500

    Remove println

--- acornui-core/src/commonMain/kotlin/com/acornui/core/focus/SimpleHighlight.kt
+++ acornui-core/src/commonMain/kotlin/com/acornui/core/focus/SimpleHighlight.kt
@@ -114,12 +114,10 @@ class SimpleFocusHighlighter(
 	)
 
 	override fun unhighlight(target: UiComponent) {
-		println("Unhighlight")
 		popUpManager.removePopUp(popUpInfo)
 	}
 
 	override fun highlight(target: UiComponent) {
-		println("Highlight")
 		highlight.highlighted = target
 		popUpManager.addPopUp(popUpInfo)
 	}
