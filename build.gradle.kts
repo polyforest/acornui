618e6977052481920accbb3dea0370c3513d3d1b
Nick <nick@polyforest.com>
2019-07-26 18:28:39 -0500

    Build reports sftp upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import com.jcraft.jsch.ChannelSftp
+import com.jcraft.jsch.JSch
+import com.jcraft.jsch.SftpATTRS
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.utils.addToStdlib.firstNotNullResult
+
 
 /*
  * Copyright 2019 Poly Forest, LLC
@@ -25,6 +29,13 @@ plugins {
 	`maven-publish`
 	idea
 }
+
+buildscript {
+	dependencies {
+		"classpath"(group = "com.jcraft", name = "jsch", version = "0.1.55")
+	}
+}
+
 
 subprojects {
 	apply {
@@ -82,4 +93,98 @@ allprojects {
 			dependsOn("cleanSnapshots")
 		}
 	}
+}
+
+
+tasks.register("publishReports") {
+	group = "publishing"
+	doLast {
+		jschSftp("bandbox.dreamhost.com") { channel ->
+			val reportDir = "testreports.acornui.com"
+			channel.deleteRecursively(reportDir)
+			subprojects.forEach { subProject ->
+				val reports = subProject.buildDir.resolve("reports")
+				if (reports.exists()) {
+					channel.uploadDir(reports, "$reportDir/${subProject.name}")
+					logger.lifecycle("Published unit test reports to http://testreports.acornui.com/${subProject.name}/tests/jvmTest/")
+				}
+			}
+		}
+	}
+}
+
+fun Project.jschSftp(host: String, inner: Project.(channel: ChannelSftp)->Unit) {
+	val jsch = JSch()
+	val ftpUsername: String = System.getenv("BANDBOX_FTP_USERNAME")
+	val ftpPassword: String = System.getenv("BANDBOX_FTP_PASSWORD")
+
+	val session = jsch.getSession(ftpUsername, host)
+	session.setConfig("StrictHostKeyChecking", "no")
+	session.setPassword(ftpPassword)
+	session.connect()
+	val channel = session.openChannel("sftp") as ChannelSftp
+	channel.connect()
+	try {
+		inner(channel)
+	} finally {
+		channel.disconnect()
+		session.disconnect()
+	}
+}
+
+fun ChannelSftp.deleteRecursively(remoteDir: String) {
+	if (isDir(remoteDir)) {
+		ls(remoteDir).forEach { entry ->
+			entry as ChannelSftp.LsEntry
+			if (!(entry.filename == "." || entry.filename == "..")) {
+				val file = "$remoteDir/${entry.filename}"
+				if (entry.attrs.isDir) {
+					deleteRecursively(file)
+				} else {
+					rm(file)
+				}
+			}
+		}
+		rmdir(remoteDir)
+	}
+}
+
+fun ChannelSftp.mkdirs(destination: String) {
+	var path = ""
+	destination.split("/").forEach {
+		path += it
+		if (!exists(path)) {
+			mkdir(path)
+		}
+		path += "/"
+	}
+}
+
+fun ChannelSftp.uploadDir(file: File, destination: String) {
+	if (!file.exists()) return
+	val children = file.listFiles()!!
+	mkdirs(destination)
+	children.forEach {
+		val childDestination = "$destination/${it.name}"
+		if (it.isDirectory) {
+			uploadDir(it, childDestination)
+		} else {
+			mkdirs(destination)
+			put(it.path, childDestination)
+		}
+	}
+}
+
+fun ChannelSftp.exists(dir: String): Boolean {
+	return statOrNull(dir) != null
+}
+
+fun ChannelSftp.isDir(dir: String): Boolean {
+	return statOrNull(dir)?.isDir == true
+}
+
+fun ChannelSftp.statOrNull(dir: String): SftpATTRS? {
+	return try {
+		stat(dir)
+	} catch (e: Throwable) { null }
 }
