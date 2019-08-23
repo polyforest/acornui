8b59645b1cf1cf0767f03705c3445ecd96f4377b
Nick <nick@polyforest.com>
2019-08-23 15:02:10 -0500

    createKotlinRuntimeClasspath

---
+++ build-libs/acornui-app-plugins/src/main/kotlin/com/acornui/build/plugins/util/createKotlinRuntimeClasspath.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019 Poly Forest, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.acornui.build.plugins.util
+
+import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilationToRunnableFiles
+import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
+
+
+fun Project.createRuntimeKotlinClasspath(compilationName: String, configure: KotlinDependencyHandler.() -> Unit): FileCollection {
+	return (kotlinExt.targets.getByName("jvm").compilations.create(compilationName) {
+		dependencies(configure)
+	} as KotlinCompilationToRunnableFiles<KotlinCommonOptions>).runtimeDependencyFiles
+}
