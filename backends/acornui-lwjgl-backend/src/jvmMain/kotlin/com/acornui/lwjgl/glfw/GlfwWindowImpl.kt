62a5d422ff401af070a2ce2f6b08e9c102dc036b
Nick <nick@polyforest.com>
2019-09-25 14:13:27 -0500

    Fix GlfwWindow alert can cause out of memory error Caused by: java.lang.OutOfMemoryError: Out of stack space.

--- backends/acornui-lwjgl-backend/src/jvmMain/kotlin/com/acornui/lwjgl/glfw/GlfwWindowImpl.kt
+++ backends/acornui-lwjgl-backend/src/jvmMain/kotlin/com/acornui/lwjgl/glfw/GlfwWindowImpl.kt
@@ -341,7 +341,7 @@ class GlfwWindowImpl(
 	override val location: Location = JvmLocation()
 
 	override fun alert(message: String) {
-		TinyFileDialogs.tinyfd_notifyPopup(null, message, "error")
+		TinyFileDialogs.tinyfd_notifyPopup(null, message.substringInRange(0, 2000), "error")
 	}
 
 	override fun dispose() {
