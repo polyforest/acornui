5ef33fa118bbd0cd2a32cdea83a87c8fd4f66020
Nick <nick@polyforest.com>
2020-01-29 13:56:46 -0600

    Attempting to use gnugpg as signatory

--- gradle/mavenPublish.gradle.kts
+++ gradle/mavenPublish.gradle.kts
@@ -121,9 +121,11 @@ the<PublishingExtension>().apply {
 
 the<SigningExtension>().apply {
 	isRequired = !isSnapshot
+	val signingKeyId: String? by project
 	val signingKey: String? by project
 	val signingPassword: String? by project
-	useInMemoryPgpKeys(signingKey, signingPassword)
+	useGpgCmd()
+	useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
 }
 
 the<de.marcphilipp.gradle.nexus.NexusPublishExtension>().apply {
