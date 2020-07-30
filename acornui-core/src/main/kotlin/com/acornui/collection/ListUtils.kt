86883c467882b6fe6ffa4a037b80326616db29a0
Nick <nick@polyforest.com>
2020-07-30 14:20:31 -0500

    Add sumByLong

--- acornui-core/src/main/kotlin/com/acornui/collection/ListUtils.kt
+++ acornui-core/src/main/kotlin/com/acornui/collection/ListUtils.kt
@@ -690,3 +690,14 @@ fun DoubleArray.mutate(builder: (DoubleArray) -> Unit): DoubleArray {
 	builder(c)
 	return c
 }
+
+/**
+ * Returns the sum of all values produced by [selector] function applied to each element in the collection.
+ */
+inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
+	var sum = 0L
+	for (element in this) {
+		sum += selector(element)
+	}
+	return sum
+}
