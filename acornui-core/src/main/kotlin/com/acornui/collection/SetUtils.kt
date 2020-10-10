2e5c6703f869d4928dcb304dc7eec8c07194f231
Nick <nick@polyforest.com>
2020-10-10 15:07:34 -0500

    Add Set.containsAny

---
+++ acornui-core/src/main/kotlin/com/acornui/collection/SetUtils.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2020 Poly Forest, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.acornui.collection
+
+/**
+ * Returns true if the intersection of the two sets is not empty.
+ */
+fun <E> Set<E>.containsAny(other: Set<E>): Boolean =
+	intersect(other).isNotEmpty()
